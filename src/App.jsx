eac9ae2b1bc5b6daaebcc43888b983eef196e381
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-03 22:04:02 +0100

    Removed default value on textarea tag

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,6 @@ function App() {
               name="description"
               rows={4}
               cols={40}
-              defaultValue="Product description"
               className="w-full px-3 py-2 border rounded"
               value={newItem.description}
               onChange={(e) =>
