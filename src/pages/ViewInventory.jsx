ae9ffab7e73128fde729339424fa1fdc8ad886f9
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-12 16:03:40 +0100

    Conditionally rendering of view inventory only if there's almost 1 element added

--- src/pages/ViewInventory.jsx
+++ src/pages/ViewInventory.jsx
@@ -17,36 +17,41 @@ export default function ViewInventory() {
 
   const thStyle = "px-6 py-3 text-left border-r border-gray-300";
   const tdStyle = "px-6 py-4 whitespace-nowrap border-r border-gray-300";
+
   return (
     <>
-      <Button onClick={handleDelete}>Delete Fields</Button>
-      <div className="overflow-x-auto">
-        <table className="w-full bg-white border border-gray-200 divide-y divide-gray-200 rounded-xl shadow-md mt-6">
-          <thead className="bg-indigo-500 text-white">
-            <tr>
-              <th className={thStyle}>Name</th>
-              <th className={thStyle}>Quantity</th>
-              <th className={thStyle}>Selling Price</th>
-              <th className={thStyle}>Cost Price</th>
-              <th className={thStyle}>Description</th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200">
-            {products &&
-              products.map((product, index) => (
-                <tr key={index}>
-                  <td className={tdStyle}>{product.name}</td>
-                  <td className={tdStyle}>{product.quantity}</td>
-                  <td className={tdStyle}>{product.sellingPrice}</td>
-                  <td className={tdStyle}>{product.costPrice}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    {product.description}
-                  </td>
+      {products && products.length > 0 && (
+        <>
+          <Button onClick={handleDelete}>Delete Fields</Button>
+          <div className="overflow-x-auto">
+            <table className="w-full bg-white border border-gray-200 divide-y divide-gray-200 rounded-xl shadow-md mt-6">
+              <thead className="bg-indigo-500 text-white">
+                <tr>
+                  <th className={thStyle}>Name</th>
+                  <th className={thStyle}>Quantity</th>
+                  <th className={thStyle}>Selling Price</th>
+                  <th className={thStyle}>Cost Price</th>
+                  <th className={thStyle}>Description</th>
                 </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {products &&
+                  products.map((product, index) => (
+                    <tr key={index}>
+                      <td className={tdStyle}>{product.name}</td>
+                      <td className={tdStyle}>{product.quantity}</td>
+                      <td className={tdStyle}>{product.sellingPrice}</td>
+                      <td className={tdStyle}>{product.costPrice}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {product.description}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </>
   );
 }
